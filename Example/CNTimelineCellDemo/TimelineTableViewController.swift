69a227b2feb5abc66fcb6d94036ad0c611100d30
Chris Ng <chrisnyw@gmail.com>
2022-04-07 12:21:01 -0700

    lower deployment target to iOS 11 by replacing UIImage(named:) instead

--- Example/CNTimelineCellDemo/TimelineTableViewController.swift
+++ Example/CNTimelineCellDemo/TimelineTableViewController.swift
@@ -11,14 +11,14 @@ import CNTimelineCell
 class TimelineTableViewController: UITableViewController {
 
   var timelineStyle: TimelineStyle = TimelineStyle()
-  let dataItem: [TimelineItem] = [TimelineItem(title: "Good morning", content: "Today is sunny day!", image: UIImage(systemName: "sun.min")),
-                                  TimelineItem(title: "08:00 am", content: "Tom wakes up", image: UIImage(systemName: "star.fill"), leftType: .start),
+  let dataItem: [TimelineItem] = [TimelineItem(title: "Good morning", content: "Today is sunny day!", image: UIImage(named: "light")),
+                                  TimelineItem(title: "08:00 am", content: "Tom wakes up", image: UIImage(named: "star"), leftType: .start),
                                   TimelineItem(title: "08:05 am", content: "Breakfast time", leftType: .spot),
-                                  TimelineItem(title: "08:30 am", content: "Mary wakes up", image: UIImage(systemName: "star.fill"), leftType: .line, rightType: .start),
-                                  TimelineItem(title: "09:00 am", content: "School time", image: UIImage(systemName: "studentdesk"), leftType: .spot, rightType: .spot),
+                                  TimelineItem(title: "08:30 am", content: "Mary wakes up", image: UIImage(named: "star"), leftType: .line, rightType: .start),
+                                  TimelineItem(title: "09:00 am", content: "School time", image: UIImage(named: "school"), leftType: .spot, rightType: .spot),
                                   TimelineItem(title: "09:00 am - 12:00pm", content: "Many lessons\nMath\nEnglish\netc...", leftType: .line, rightType: .line),
-                                  TimelineItem(title: "12:05 pm", content: "Lunch together", image: UIImage(systemName: "fork.knife"), leftType: .spot, rightType: .spot),
-                                  TimelineItem(title: "01:00 pm", content: "Outdoor activities, play games in sports court.", image: UIImage(systemName: "sportscourt"), leftType: .spot, rightType: .spot),
+                                  TimelineItem(title: "12:05 pm", content: "Lunch together", image: UIImage(named: "restaurant"), leftType: .spot, rightType: .spot),
+                                  TimelineItem(title: "01:00 pm", content: "Outdoor activities, play games in sports court.", image: UIImage(named: "sports_basketball"), leftType: .spot, rightType: .spot),
 
                                   TimelineItem(title: "until 03:00 pm", content: "Afternoon lesson", leftType: .line, rightType: .line),
                                   TimelineItem(title: "03:00 pm", content: "Free time", leftType: .spot, rightType: .spot),
@@ -27,11 +27,11 @@ class TimelineTableViewController: UITableViewController {
                                   TimelineItem(title: "04:00 pm", content: "Play in playground", leftType: .none, rightType: .spot),
                                   TimelineItem(title: "04:15 pm", content: "Wake up from nap", leftType: .start, rightType: .line),
                                   TimelineItem(title: "05:00 pm", content: "Play TV games", leftType: .spot, rightType: .line),
-                                  TimelineItem(title: "07:00 pm", content: "Dinner", image: UIImage(systemName: "fork.knife"), leftType: .line, rightType: .spot),
-                                  TimelineItem(title: "08:00 pm", content: "Dinner", image: UIImage(systemName: "fork.knife"), leftType: .spot, rightType: .line),
-                                  TimelineItem(title: "10:00 pm", content: "Goto sleep", image: UIImage(systemName: "bed.double"), leftType: .line, rightType: .end),
-                                  TimelineItem(title: "10:30 pm", content: "Goto sleep", image: UIImage(systemName: "bed.double.fill"), leftType: .end, rightType: .none),
-                                  TimelineItem(title: "Awesome", content: "That's all for the day", image: UIImage(systemName: "sparkles")),]
+                                  TimelineItem(title: "07:00 pm", content: "Dinner", image: UIImage(named: "restaurant"), leftType: .line, rightType: .spot),
+                                  TimelineItem(title: "08:00 pm", content: "Dinner", image: UIImage(named: "restaurant"), leftType: .spot, rightType: .line),
+                                  TimelineItem(title: "10:00 pm", content: "Goto sleep", image: UIImage(named: "bed"), leftType: .line, rightType: .end),
+                                  TimelineItem(title: "10:30 pm", content: "Goto sleep", image: UIImage(named: "single_bed"), leftType: .end, rightType: .none),
+                                  TimelineItem(title: "Awesome", content: "That's all for the day", image: UIImage(named: "auto_awesome")),]
   
   override func viewDidLoad() {
     super.viewDidLoad()
@@ -41,6 +41,18 @@ class TimelineTableViewController: UITableViewController {
     
     let timelineRight = LineStyle(lineColor: .red)
     timelineStyle = TimelineStyle(rightLineStyle: timelineRight)
+    
+    let container = UIView()
+    let rect = UIView()
+    
+    container.addSubview(rect)
+    container.frame = CGRect(x: 0, y: 0, width: 500, height: 500)
+    rect.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+    print(rect.frame)
+    print(rect.bounds)
+    rect.transform = CGAffineTransform(scaleX: 2, y: 2)
+    print(rect.frame)
+    print(rect.bounds)
   }
 
   // MARK: - Table view data source
@@ -78,7 +90,7 @@ class TimelineTableViewController: UITableViewController {
     }
 
     cell.setCell(item: timelineItem, style: timelineStyle)
-    
+
     return cell
   }
   
--- Package.swift
+++ Package.swift
@@ -15,7 +15,7 @@ let excludedFiles = [
 let package = Package(
   name: packageName,
   platforms: [
-    .iOS(.v13)
+    .iOS(.v11)
   ],
   products: [
     // Products define the executables and libraries a package produces, and make them visible to other packages.
